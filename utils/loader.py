14ada3c7d3ddbf3929037d4581da33b3cbf6ae33
cassildobez2-star <cassildobez2@gmail.com>
2026-02-17 14:04:30 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,1 +1,10 @@
+from sources.toonbr import ToonBrSource
+from sources.mangaonline import MangaOnlineSource
 
+SOURCES = {
+    "toonbr": ToonBrSource(),
+    "mangaonline": MangaOnlineSource(),
+}
+
+def get_source(name):
+    return SOURCES.get(name)
